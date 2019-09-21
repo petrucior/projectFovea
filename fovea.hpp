a4d685ec8b93f44081487b202157b3bf0d152558
petrucior <petrucior@gmail.com>
2019-09-21 18:09:05 -0300

    Creating test file to fovea
    This commit contains the creation of fovea structure with extraction
    of ORB feature using MRMF, but isn't well defined the struct because
    it's necessary build another class that represent features.

--- fovea.hpp
+++ fovea.hpp
@@ -30,10 +30,17 @@
 
 #include <iostream> //std::cout, std::endl
 #include <vector> //std::vector
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/features2d/features2d.hpp"
 #include "level.hpp" //std::vector< Level >
 #ifdef _OPENMP
 #include <omp.h> //#pragma omp parallel for
 #endif
+
+#define MRMF 0 ///< Identify the use of MRMF method
+#define MMF 1 ///< Identify the use of MMF method
 
 /**
  * \defgroup ProjectFovea Project Fovea
@@ -54,18 +61,77 @@ public:
   // Methods
   //
   /**
+   * \fn Fovea(cv::Mat img, int m, T w, T f)
+   *
+   * \brief Constructor default of fovea class.
+   * This constructor create a fovea associeted to an image.
+   * This approcah can be applied to associate the fovea structure
+   * to unique image. 
+   *
+   * \param img - Image to be foveated
+   * \param k - Level of fovea
+   * \param m - Number levels of fovea
+   * \param w - Size of levels
+   * \param f - Position (x, y) of the fovea
+   */
+  Fovea(cv::Mat img, int m, T w, T f);
+  
+  /**
    * \fn Fovea(int m, T w, T u, T f)
    *
-   * \brief Constructor default of fovea class. THis method was 
-   * built 
+   * \brief Constructor default of fovea class.
+   * This constructor create a fovea associated to image parameters.
+   * This approach can be used and explored by robots or systems
+   * which doesn't depends of the different parameters or device.  
    *
    * \param k - Level of fovea
    * \param m - Number levels of fovea
    * \param w - Size of levels
    * \param u - Size of image
+   * \param f - Position (x, y) of the fovea
    */
   Fovea(int m, T w, T u, T f);
+  
+  /**
+   * \fn ~Fovea()
+   *
+   * \brief Destructor default of fovea class.
+   */
+  ~Fovea();
+  
+  /**
+   * \fn void updateFovea(int m, T w, T u, T f)
+   *
+   * \brief This method update the fovea structure
+   * and can be thought in update only what need to 
+   * be changed, in other words, the update occors
+   * only in shape of level. 
+   * It is necessary to be implemented!
+   *
+   * \param m - Number levels of fovea
+   * \param w - Size of levels
+   * \param u - Size of image
+   * \param f - Position (x, y) of the fovea
+   */
+  void updateFovea(int m, T w, T u, T f);
 
+  /**
+   * \fn bool foveatedFeatures( cv::Mat img, Feature feature, int code )
+   * \fn bool computeAndExtractFeatures( cv::Mat img, Feature feature, int code )
+   *
+   * \brief This method compute and extract features 
+   * of foveated structure using MRMF or MMF.
+   *
+   * \param img - Image to be foveated
+   * \param code - This code indicates which method
+   * to foveation will be used. If code is zero, then
+   * MRMF is chosen, otherwise MMF.
+   *
+   * \return True if was done computed and extracted
+   * features and False otherwise.
+   */
+  bool foveatedFeatures( cv::Mat img, /*Feature feature,*/ int code );
+    
 private:
   //
   // Methods
@@ -85,37 +151,150 @@ private:
   //
   // Attributes
   //
-  std::vector< Level > levels; ///< List of levels
+  std::vector< Level< T > > levels; ///< List of levels
   int m; ///< Number levels of fovea
   T w; ///< Size of levels
   T u; ///< Size of image
   T f; ///< Position (x, y) to build the fovea
-  
 };
 
 #endif
 
-
 /**
- * \fn Fovea(int m, T w, T u, T f);
+ * \fn Fovea(cv::Mat img, int m, T w, T f)
  *
- * \brief Constructor default of fovea class
+ * \brief Constructor default of fovea class.
  *
+ * \param img - Image to be foveated
  * \param k - Level of fovea
  * \param m - Number levels of fovea
  * \param w - Size of levels
- * \param u - Size of image
+ * \param f - Position (x, y) of the fovea
  */
-Fovea::Fovea(int m, T w, T u, T f){
+template <typename T>
+Fovea< T >::Fovea(cv::Mat img, int m, T w, T f){
+  T u = T( img.cols, img.rows );
   this->checkParameters( m, w, u, f );
 #ifdef _OPENMP
 #pragma omp parallel for // reference http://ppc.cs.aalto.fi/ch3/nested/
 #endif
   for ( int k = 0; k < m; k++ ){
-    Level l = new Level( k, m, w, u, f );
+    Level< T > l( k, m, w, u, f );
     levels.push_back( l );
   }
 }
+
+/**
+ * \fn Fovea(int m, T w, T u, T f)
+ *
+ * \brief Constructor default of fovea class.
+ * This constructor create a fovea associated to image parameters.
+ * This approach can be used and explored by robots or systems
+ * which doesn't depends of the different parameters or device.  
+ *
+ * \param k - Level of fovea
+ * \param m - Number levels of fovea
+ * \param w - Size of levels
+ * \param u - Size of image
+ * \param f - Position (x, y) of the fovea
+ */
+template <typename T>
+Fovea< T >::Fovea(int m, T w, T u, T f){
+  this->checkParameters( m, w, u, f );
+#ifdef _OPENMP
+#pragma omp parallel for // reference http://ppc.cs.aalto.fi/ch3/nested/
+#endif
+  for ( int k = 0; k < m; k++ ){
+    Level< T > l( k, m, w, u, f );
+    levels.push_back( l );
+  }
+} 
+
+/**
+ * \fn ~Fovea()
+ *
+ * \brief Destructor default of fovea class.
+ */
+template <typename T>
+Fovea< T >::~Fovea(){
+  std::vector< Level< T > >().swap( this->levels ); // Free the memory
+}
+
+/**
+ * \fn void updateFovea(int m, T w, T u, T f)
+ *
+ * \brief This method update the fovea structure
+ * and can be thought in update only what need to 
+ * be changed, in other words, the update occors
+ * only in shape of level. 
+ * It is necessary to be implemented!
+ *
+ * \param m - Number levels of fovea
+ * \param w - Size of levels
+ * \param u - Size of image
+ * \param f - Position (x, y) of the fovea
+ */
+template <typename T>
+void 
+Fovea< T >::updateFovea(int m, T w, T u, T f){
+  this->checkParameters( m, w, u, f );
+  if ( levels.size() > 0 ){
+    // cleaning vector levels, but conserving the first level
+    levels.erase( levels.begin() + 1, levels.end() );
+    levels.shrink_to_fit();
+  }
+#ifdef _OPENMP
+#pragma omp parallel for // reference http://ppc.cs.aalto.fi/ch3/nested/
+#endif
+  for ( int k = levels.size(); k < m; k++ ){
+    Level< T > l( k, m, w, u, f );
+    levels.push_back( l );
+  }
+}
+
+/**
+ * \fn bool foveatedFeatures( cv::Mat img, Feature feature, int code )
+ *
+ * \brief This method compute and extract features 
+ * of foveated structure using MRMF or MMF.
+ *
+ * \param img - Image to be foveated
+ * \param code - This code indicates which method
+ * to foveation will be used. If code is zero, then
+ * MRMF is chosen, otherwise MMF.
+ *
+ * \return True if was done computed and extracted
+ * features and False otherwise.
+ */
+template <typename T>
+bool 
+Fovea< T >::foveatedFeatures( cv::Mat img, /*Feature feature,*/ int code ){
+  std::vector< cv::KeyPoint > kp;
+  cv::Mat dp, output;
+  cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
+  cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
+  switch ( code ){
+  case MRMF:
+    std::cout << "MRMF actived" << std::endl;
+    for ( int i = 0; i < levels.size(); i++ ){
+      cv::Mat level = levels[i].getLevel( img );
+      //cv::imshow( "level", level );
+      //cv::waitKey( 0 );
+      detector->detect ( level, kp );
+      descriptor->compute ( level, kp, dp );
+      cv::drawKeypoints( level, kp, output, cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT );
+      cv::imshow( "keypoints", output);
+      cv::waitKey( 0 );
+    }
+    break;
+  case MMF:
+    std::cout << "MMF actived" << std::endl;
+    break;
+  default:
+    std::cout << "Code unknown" << std::endl;
+  }
+  return true;
+} 
 
 /**
  * \fn void checkParameters( int m, T w, T u, T f )
@@ -127,8 +306,9 @@ Fovea::Fovea(int m, T w, T u, T f){
  * \param w - Size of levels
  * \param u - Size of image
  */
-void 
-Fovea::checkParameters( int m, T w, T u, T f ){
+template <typename T>
+void
+Fovea< T >::checkParameters( int m, T w, T u, T f ){
   // Verify if there is the minimun one level
   assert( m >= 1 );
   // Verify if w is bigger than zero and lower than image size
---
+++ tests/testfovea.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+//#include "../shape.hpp"
+//#include "../rectangle.hpp"
+//#include "../level.hpp"
+#include "../fovea.hpp"
+
+using namespace cv;
+
+int main( int argc, char** argv ){
+  // Testing constructor and print functions
+  std::cout << argv[1] << std::endl;
+  Mat img = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+  //Fovea< Point > f( img, 4, Point(30, 30), Point( 0, 0 ) );
+  Fovea< Point > f( 4, Point(30, 30), Point( img.cols, img.rows ), Point( 0, 0 ) );
+  f.foveatedFeatures( img, MRMF );
+  //img = l.get( img );
+  //imshow( "image", img );
+  //waitKey( 0 );
+  return 0;
+}
